9a0cbab06820402b0612b7adf84ae32e2bdaa584
Kinnaird McQuade <kmcquade@salesforce.com>
2020-06-25 09:43:14 -0400

    Move report into its own class

--- test/command/test_scan.py
+++ test/command/test_scan.py
@@ -3,10 +3,11 @@ import os
 import json
 import webbrowser
 from cloudsplaining.command.scan import scan_account_authorization_details
-from cloudsplaining.output.html_report import generate_html_report
 from cloudsplaining.shared.constants import DEFAULT_EXCLUSIONS_CONFIG
 from cloudsplaining.shared.exclusions import DEFAULT_EXCLUSIONS, Exclusions
 from cloudsplaining.shared.validation import check_authorization_details_schema
+from cloudsplaining.output.report import HTMLReport
+from cloudsplaining.scan.authorization_details import AuthorizationDetails
 
 example_results_file = os.path.abspath(os.path.join(
     os.path.dirname(__file__),
@@ -37,34 +38,89 @@ with open(example_principal_policy_mapping_file) as json_file:
 
 class PolicyFileTestCase(unittest.TestCase):
     def test_output_html_output_as_string(self):
-        account_name = "fake"
-        account_metadata = {
-            "account_name": account_name,
-            "account_id": "000011112222",
-            "customer_managed_policies": 20,  # Fake value
-            "aws_managed_policies": 30,  # Fake value
-        }
+        example_authz_details_file = os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__),
+                os.path.pardir,
+                "files",
+                "example-authz-details.json",
+            )
+        )
+        with open(example_authz_details_file, "r") as json_file:
+            cfg = json.load(json_file)
+            decision = check_authorization_details_schema(cfg)
+        self.assertTrue(decision)
+        # TODO: These values are just for testing
+        account_authorization_details_cfg = cfg
+        exclusions = DEFAULT_EXCLUSIONS
 
-        rendered_html_report = generate_html_report(
-            account_metadata,
-            example_results,
-            example_principal_policy_mapping,
-            DEFAULT_EXCLUSIONS_CONFIG
+        authorization_details = AuthorizationDetails(account_authorization_details_cfg)
+        results = authorization_details.missing_resource_constraints(
+            exclusions, modify_only=True
         )
-        # print(rendered_html_report)
-        output_directory = os.path.abspath(os.path.join(
-            os.path.dirname(__file__),
-            os.path.pardir,
-            os.path.pardir,
-        )
-        )
-        html_output_file = os.path.join(output_directory, f"iam-report-{account_name}.html")
-        with open(html_output_file, "w") as f:
-            f.write(rendered_html_report)
 
-        print(f"Wrote HTML results to: {html_output_file}")
-        # url = "file://%s" % os.path.abspath(html_output_file)
+        principal_policy_mapping = authorization_details.principal_policy_mapping
+        # For the IAM Principals tab, add on risk stats per principal
+        for principal_policy_entry in principal_policy_mapping:
+            for finding in results:
+                if principal_policy_entry.get("PolicyName").lower() == finding.get("PolicyName").lower():
+                    principal_policy_entry["Actions"] = len(finding["Actions"])
+                    principal_policy_entry["PrivilegeEscalation"] = len(
+                        finding["PrivilegeEscalation"]
+                    )
+                    principal_policy_entry["DataExfiltrationActions"] = len(
+                        finding["DataExfiltrationActions"]
+                    )
+                    principal_policy_entry["PermissionsManagementActions"] = len(
+                        finding["PermissionsManagementActions"]
+                    )
+                    principal_name = principal_policy_entry["Principal"]
+                    # Customer Managed Policies
+                    if finding.get("Type") == "Policy" and finding.get(
+                        "ManagedBy") == "Customer" and principal_policy_entry.get("Type") != "Policy":
+                        if "Principals" not in finding:
+                            finding["Principals"] = [principal_name]
+                        else:
+                            if principal_name not in finding["Principals"]:
+                                finding["Principals"].append(principal_name)
+
+                    # AWS Managed Policies
+                    if finding.get("Type") == "Policy" and finding.get("ManagedBy") == "AWS":
+                        if "Principals" not in finding:
+                            finding["Principals"] = [principal_name]
+                        else:
+                            if principal_name not in finding["Principals"]:
+                                finding["Principals"].append(principal_name)
+
+        # Lazy method to get an account ID
+        account_id = None
+        for item in results:
+            if item["ManagedBy"] == "Customer":
+                account_id = item["AccountID"]
+                break
+
+        html_report = HTMLReport(
+            account_id=account_id,
+            account_name="CHANGEME",
+            results=results,
+            exclusions_cfg=exclusions,
+            principal_policy_mapping=principal_policy_mapping
+        )
+        rendered_report = html_report.get_html_report()
+        # print(rendered_report)
+        # test_report_path = os.path.join(
+        #     os.getcwd(),
+        #     os.path.pardir,
+        #     os.path.pardir,
+        #     "tmp",
+        #     "testing_new_html_report.html"
+        # )
+        # with open(test_report_path, "w") as file:
+        #     file.write(rendered_report)
+        # print("Opening the HTML report")
+        # url = "file://%s" % os.path.abspath(test_report_path)
         # webbrowser.open(url, new=2)
+
 
     def test_scan_authz_details_and_output_html_as_string(self):
         example_authz_details_file = os.path.abspath(
@@ -81,7 +137,7 @@ class PolicyFileTestCase(unittest.TestCase):
         self.assertTrue(decision)
 
         rendered_html_report = scan_account_authorization_details(
-            cfg, DEFAULT_EXCLUSIONS, output=os.getcwd(), all_access_levels=False, skip_open_report=True, account_name="Something",
+            cfg, DEFAULT_EXCLUSIONS, output_directory=os.getcwd(), account_name="Something",
             write_data_files=False
         )
         print(rendered_html_report)
