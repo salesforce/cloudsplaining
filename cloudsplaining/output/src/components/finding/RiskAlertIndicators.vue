885bf9d19e533dc3b03f9d61adf64e0e7666520d
Kinnaird McQuade <kmcquade@salesforce.com>
2020-10-11 20:54:35 -0400

    Updated risk alert indicator and definitions

--- cloudsplaining/output/src/components/finding/RiskAlertIndicators.vue
+++ cloudsplaining/output/src/components/finding/RiskAlertIndicators.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <!--Alert for Risk Type: Privilege Escalation Exposure-->
+        <!--Alert boxes for different risks-->
         <div v-for="riskType in highRisksToDisplayAlertsFor" :key="riskType">
             <template v-if="findings(policyId, riskType).length > 0">
                 <div v-bind:class="'alert alert-' + getRiskAlertIndicatorColor(riskType) + ' popovers'" data-html="true" data-placement="top"
