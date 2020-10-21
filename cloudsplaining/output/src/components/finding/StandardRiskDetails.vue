b68cadf643ca8166c55e9c4696cf772bcbb30119
Kinnaird McQuade <kmcquade@salesforce.com>
2020-10-21 16:28:05 -0400

    Add inline explanations for each risk type

--- cloudsplaining/output/src/components/finding/StandardRiskDetails.vue
+++ cloudsplaining/output/src/components/finding/StandardRiskDetails.vue
@@ -13,12 +13,31 @@
                 <div class="panel-collapse collapse"
                      v-bind:id="`${inlineOrManaged.toLowerCase()}-policy-${policyId}-${convertStringToKebabCase(risk.risk_type)}`">
                     <div class="card-body">
-                        <b-button v-b-modal="`${inlineOrManaged.toLowerCase()}-policy-${policyId}-${convertStringToKebabCase(risk.risk_type)}-instructions`">Show Instructions</b-button>
-                        <b-modal v-bind:id="`${inlineOrManaged.toLowerCase()}-policy-${policyId}-${convertStringToKebabCase(risk.risk_type)}-instructions`">
+                        <!--What should I do?-->
+                        <b-button squared variant="link" v-b-modal="`${inlineOrManaged.toLowerCase()}-policy-${policyId}-${convertStringToKebabCase(risk.risk_type)}-what-should-i-do`">What should I do?</b-button>
+                        <b-modal v-bind:id="`${inlineOrManaged.toLowerCase()}-policy-${policyId}-${convertStringToKebabCase(risk.risk_type)}-what-should-i-do`">
                             <p>
-                                <span v-html="instructionsGeneral"></span>
+                                <span v-html="whatShouldIDoDescription"></span>
                             </p>
                         </b-modal>
+                        <!--Triaging: Identifying False Positives-->
+                        <b-button squared variant="link" v-b-modal="`${inlineOrManaged.toLowerCase()}-policy-${policyId}-${convertStringToKebabCase(risk.risk_type)}-identifying-false-positives`">How do I identify False Positives?</b-button>
+                        <b-modal v-bind:id="`${inlineOrManaged.toLowerCase()}-policy-${policyId}-${convertStringToKebabCase(risk.risk_type)}-identifying-false-positives`">
+                            <p>
+                                <span v-html="identifyingFalsePositivesDescription"></span>
+                            </p>
+                        </b-modal>
+                        <!--How do I validate results?-->
+                        <b-button squared variant="link" v-b-modal="`${inlineOrManaged.toLowerCase()}-policy-${policyId}-${convertStringToKebabCase(risk.risk_type)}-validating-fixed-policy`">How do I fix it?</b-button>
+                        <b-modal v-bind:id="`${inlineOrManaged.toLowerCase()}-policy-${policyId}-${convertStringToKebabCase(risk.risk_type)}-validating-fixed-policy`">
+                            <p>
+                                <span v-html="howDoIValidateResultsDescription"></span>
+                            </p>
+                        </b-modal>
+                        <br>
+                        <br>
+                        <span v-html="getRiskDescription(risk.risk_type)"></span>
+                        <span>Actions/services:</span>
 <pre><code>
 {{ JSON.parse(JSON.stringify(findings(policyId, risk.risk_type), undefined, '\t')) }}
 </code></pre>
@@ -40,9 +59,29 @@
         linkify: true,
         typographer: true
     });
-    import instructionsGeneralRaw from '../../assets/instructions-general.md'
+    import resourceExposureRaw from '../../assets/definition-resource-exposure.md'
+    import privilegeEscalationRaw from '../../assets/definition-privilege-escalation.md'
+    import dataExfiltrationRaw from '../../assets/definition-data-exfiltration.md'
+    import infrastructureModificationRaw from '../../assets/definition-infrastructure-modification.md'
+    import credentialsExposureRaw from '../../assets/definition-credentials-exposure.md'
+    import serviceWildcardRaw from '../../assets/definition-service-wildcard.md'
+    import assumableByComputeServiceRaw from '../../assets/definition-assumable-by-compute-service.md'
 
-    const instructionsGeneral = md.render(instructionsGeneralRaw)
+    const resourceExposureDescription = md.render(resourceExposureRaw)
+    const privilegeEscalationDescription = md.render(privilegeEscalationRaw)
+    const dataExfiltrationDescription = md.render(dataExfiltrationRaw)
+    const infrastructureModificationDescription = md.render(infrastructureModificationRaw)
+    const credentialsExposureDescription = md.render(credentialsExposureRaw)
+    const serviceWildcardDescription = md.render(serviceWildcardRaw)
+    const assumableByComputeServiceDescription = md.render(assumableByComputeServiceRaw)
+
+    import whatShouldIDoRaw from '../../assets/what-should-i-do.md'
+    import identifyingFalsePositivesDescriptionRaw from '../../assets/identifying-false-positives.md'
+    import howDoIValidateResultsRaw from '../../assets/how-do-i-validate-results.md'
+
+    const whatShouldIDoDescription = md.render(whatShouldIDoRaw)
+    const identifyingFalsePositivesDescription = md.render(identifyingFalsePositivesDescriptionRaw)
+    const howDoIValidateResultsDescription = md.render(howDoIValidateResultsRaw)
 
     export default {
         name: "StandardRiskDetails",
@@ -69,9 +108,15 @@
             riskDetailsToDisplay() {
                 return glossary.getRiskDetailsToDisplay()
             },
-            instructionsGeneral() {
-                return instructionsGeneral
-            }
+            whatShouldIDoDescription() {
+                return whatShouldIDoDescription
+            },
+            identifyingFalsePositivesDescription() {
+                return identifyingFalsePositivesDescription
+            },
+            howDoIValidateResultsDescription() {
+                return howDoIValidateResultsDescription
+            },
         },
         methods: {
             findings: function (policyId, riskType) {
@@ -86,6 +131,35 @@
             },
             convertStringToKebabCase: function(string) {
                 return otherUtil.convertStringToKebabCase(string)
+            },
+            getRiskDefinition: function (riskType) {
+                return glossary.getRiskDefinition(riskType)
+            },
+            getRiskDescription: function (riskType) {
+                if (riskType === "ResourceExposure") {
+                    return resourceExposureDescription
+                }
+                else if (riskType === "PrivilegeEscalation") {
+                    return privilegeEscalationDescription
+                }
+                else if (riskType === "DataExfiltration") {
+                    return dataExfiltrationDescription
+                }
+                else if (riskType === "InfrastructureModification") {
+                    return infrastructureModificationDescription
+                }
+                else if (riskType === "AssumableByComputeService") {
+                    return assumableByComputeServiceDescription
+                }
+                else if (riskType === "ServiceWildcard") {
+                    return serviceWildcardDescription
+                }
+                else if (riskType === "CredentialsExposure") {
+                    return credentialsExposureDescription
+                }
+                else {
+                    return ""
+                }
             }
         }
     }
