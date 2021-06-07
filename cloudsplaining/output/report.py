ec0b5e6e9ed9582dcbac7a72210252ef6eaef4c4
Anton Grübel <anton.gruebel@gmail.com>
2021-06-07 20:06:52 +0200

    Add type hints to shared and output (#206)
    * Add type hints to shared
    
    * Add type hints to output

--- cloudsplaining/output/report.py
+++ cloudsplaining/output/report.py
@@ -1,17 +1,25 @@
 """Creates the HTML Reports"""
-import os
 import json
 import datetime
+from pathlib import Path
+from typing import Dict, Any
+
 from jinja2 import Environment, FileSystemLoader
 from cloudsplaining.bin.version import __version__
 
-app_bundle_path = os.path.join(os.path.dirname(__file__), "dist", "js", "index.js")
+app_bundle_path = Path(__file__).parent / "dist" / "js" / "index.js"
 
 
 class HTMLReport:
     """Inject the JS files and report results into the final HTML report"""
 
-    def __init__(self, account_id, account_name, results, minimize=False):
+    def __init__(
+        self,
+        account_id: str,
+        account_name: str,
+        results: Dict[str, Dict[str, Any]],
+        minimize: bool = False,
+    ) -> None:
         self.account_name = account_name
         self.account_id = account_id
         self.report_generated_time = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -19,7 +27,7 @@ class HTMLReport:
         self.results = f"var iam_data = {json.dumps(results, default=str)}"
 
     @property
-    def app_bundle(self):
+    def app_bundle(self) -> str:
         """The Cloudsplaining Javascript application code should be loaded either from the CDN or locally,
         depending on if the user specified the --minimize option"""
         if self.minimize:
@@ -27,13 +35,12 @@ class HTMLReport:
             bundle = f'<script type="text/javascript" src="{js_url}"></script>'
             return bundle
         else:
-            with open(app_bundle_path, "r", encoding="utf-8") as f:
-                bundle_content = f.read()
+            bundle_content = app_bundle_path.read_text(encoding="utf-8")
             bundle = f'<script type="text/javascript">\n{bundle_content}\n</script>'
             return bundle
 
     @property
-    def vendor_bundle(self):
+    def vendor_bundle(self) -> str:
         """The Javascript vendor bundle should be loaded either from the CDN or locally,
         depending on if the user specified the --minimize option"""
 
@@ -43,12 +50,11 @@ class HTMLReport:
             return bundle
         else:
             vendor_bundle_path = get_vendor_bundle_path()
-            with open(vendor_bundle_path, "r", encoding="utf-8") as f:
-                bundle_content = f.read()
+            bundle_content = vendor_bundle_path.read_text(encoding="utf-8")
             bundle = f'<script type="text/javascript">\n{bundle_content}\n</script>'
             return bundle
 
-    def get_html_report(self):
+    def get_html_report(self) -> str:
         """Returns the rendered HTML report"""
         template_contents = dict(
             vendor_bundle_js=self.vendor_bundle,
@@ -61,24 +67,18 @@ class HTMLReport:
             report_generated_time=str(self.report_generated_time),
             cloudsplaining_version=__version__,
         )
-        template_path = os.path.join(os.path.dirname(__file__))
+        template_path = Path(__file__).parent
         env = Environment(loader=FileSystemLoader(template_path))  # nosec
         template = env.get_template("template.html")
         return template.render(t=template_contents)
 
 
-def get_vendor_bundle_path():
+def get_vendor_bundle_path() -> Path:
     """Finds the vendored javascript bundle even if it has a hash suffix"""
-    vendor_bundle_directory = os.path.join(os.path.dirname(__file__), "dist", "js")
-    file_list = [
-        f
-        for f in os.listdir(vendor_bundle_directory)
-        if os.path.isfile(os.path.join(vendor_bundle_directory, f))
+    vendor_bundle_directory = Path(__file__).parent / "dist" / "js"
+    file_list_with_full_path = [
+        file.absolute()
+        for file in vendor_bundle_directory.glob("*.js")
+        if file.name.startswith("chunk-vendors.")
     ]
-    file_list_with_full_path = []
-    for file in file_list:
-        if file.endswith(".js") and file.startswith("chunk-vendors."):
-            file_list_with_full_path.append(
-                os.path.abspath(os.path.join(vendor_bundle_directory, file))
-            )
     return file_list_with_full_path[0]
--- cloudsplaining/shared/aws_login.py
+++ cloudsplaining/shared/aws_login.py
@@ -1,58 +1,78 @@
 """AWS Login utilities"""
 import os
 import logging
+from typing import Optional, List, Tuple
+
 import boto3
 from botocore.config import Config
+
 logger = logging.getLogger(__name__)
 
 
-def get_boto3_client(service: str, profile: str = None, region: str = "us-east-1") -> boto3.Session.client:
+def get_boto3_client(
+    service: str, profile: Optional[str] = None, region: str = "us-east-1"
+) -> boto3.Session.client:
     """Get a boto3 client for a given service"""
-    logging.getLogger('botocore').setLevel(logging.CRITICAL)
+    logging.getLogger("botocore").setLevel(logging.CRITICAL)
     session_data = {"region_name": region}
     if profile:
         session_data["profile_name"] = profile
     session = boto3.Session(**session_data)
 
     config = Config(connect_timeout=5, retries={"max_attempts": 10})
-    if os.environ.get('LOCALSTACK_ENDPOINT_URL'):
-        client = session.client(service, config=config, endpoint_url=os.environ.get('LOCALSTACK_ENDPOINT_URL'))
+    if os.environ.get("LOCALSTACK_ENDPOINT_URL"):
+        client = session.client(
+            service,
+            config=config,
+            endpoint_url=os.environ.get("LOCALSTACK_ENDPOINT_URL"),
+        )
     else:
         client = session.client(service, config=config)
-    logger.debug(f"{client.meta.endpoint_url} in {client.meta.region_name}: boto3 client login successful")
+    logger.debug(
+        f"{client.meta.endpoint_url} in {client.meta.region_name}: boto3 client login successful"
+    )
     return client
 
 
-def get_boto3_resource(service: str, profile: str = None, region: str = "us-east-1") -> boto3.Session.resource:
+def get_boto3_resource(
+    service: str, profile: Optional[str] = None, region: str = "us-east-1"
+) -> boto3.Session.resource:
     """Get a boto3 resource for a given service"""
-    logging.getLogger('botocore').setLevel(logging.CRITICAL)
+    logging.getLogger("botocore").setLevel(logging.CRITICAL)
     session_data = {"region_name": region}
     if profile:
         session_data["profile_name"] = profile
     session = boto3.Session(**session_data)
 
-    resource = session.resource(service)
+    config = Config(connect_timeout=5, retries={"max_attempts": 10})
+    resource = session.resource(service, config=config)
     return resource
 
 
 def get_current_account_id(sts_client: boto3.Session.client) -> str:
     """Get the current account ID"""
     response = sts_client.get_caller_identity()
-    current_account_id = response.get("Account")
+    current_account_id: str = response.get("Account", "")
     return current_account_id
 
 
-def get_available_regions(service: str):
+def get_available_regions(service: str) -> List[str]:
     """AWS exposes their list of regions as an API. Gather the list."""
-    regions = boto3.session.Session().get_available_regions(service)
-    logger.debug("The service %s does not have available regions. Returning us-east-1 as default")
+    regions: List[str] = boto3.session.Session().get_available_regions(service)
+    logger.debug(
+        "The service %s does not have available regions. Returning us-east-1 as default"
+    )
     if not regions:
         regions = ["us-east-1"]
     return regions
 
 
-def get_target_account_credentials(target_account_role_name: str, target_account_id: str,
-                                   role_session_name: str = "Cloudsplaining", profile: str = None):
+def get_target_account_credentials(
+    target_account_role_name: str,
+    target_account_id: str,
+    role_session_name: str = "Cloudsplaining",
+    profile: Optional[str] = None,
+) -> Tuple[str, str, str]:
     """
     Get a boto3 client for a given AWS service
 
@@ -68,15 +88,15 @@ def get_target_account_credentials(target_account_role_name: str, target_account
         session_data["profile_name"] = profile
     session = boto3.Session(**session_data)
     config = Config(connect_timeout=5, retries={"max_attempts": 10})
-    sts_client = session.client('sts', config=config)
+    sts_client = session.client("sts", config=config)
 
     acct_b = sts_client.assume_role(
         RoleArn=f"arn:aws:iam::{target_account_id}:role/{target_account_role_name}",
-        RoleSessionName=role_session_name
+        RoleSessionName=role_session_name,
     )
 
-    aws_access_key_id = acct_b['Credentials']['AccessKeyId']
-    aws_secret_access_key = acct_b['Credentials']['SecretAccessKey']
-    aws_session_token = acct_b['Credentials']['SessionToken']
+    aws_access_key_id = acct_b["Credentials"]["AccessKeyId"]
+    aws_secret_access_key = acct_b["Credentials"]["SecretAccessKey"]
+    aws_session_token = acct_b["Credentials"]["SessionToken"]
 
     return aws_access_key_id, aws_secret_access_key, aws_session_token
