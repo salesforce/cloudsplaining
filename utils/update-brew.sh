816c86dd2bd9b8d48bb8874d1bdf51e73dba4a4c
Kinnaird McQuade <kmcquade@users.noreply.github.com>
2021-08-16 15:23:09 -0700

    Fix homebrew scripts (#218)
    * Fix homebrew scripts
    
    * Make pylint happy

--- utils/update-brew.sh
+++ utils/update-brew.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -x
 python3 -m venv ./venv && source venv/bin/activate
-pip3 install -r requirements.txt
-pip3 install -r requirements-dev.txt
-invoke build.build-package
+
+pip install homebrew-pypi-poet
+pip install cloudsplaining -U
+
+
 pip uninstall -r requirements.txt -y
 pip uninstall -r requirements-dev.txt -y
 pip install homebrew-pypi-poet
