b2bd4e776fa46888a56de84a717a41936d69e050
gruebel <anton.gruebel@gmail.com>
2025-01-06 23:41:34 +0100

    drop Python 3.8 support

--- cloudsplaining/scan/group_details.py
+++ cloudsplaining/scan/group_details.py
@@ -3,11 +3,9 @@
 from __future__ import annotations
 
 import json
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 from cloudsplaining.scan.inline_policy import InlinePolicy
-from cloudsplaining.scan.managed_policy_detail import ManagedPolicyDetails
-from cloudsplaining.scan.statement_detail import StatementDetail
 from cloudsplaining.shared import utils
 from cloudsplaining.shared.exceptions import NotFoundException
 from cloudsplaining.shared.exclusions import DEFAULT_EXCLUSIONS, Exclusions
@@ -17,6 +15,10 @@ from cloudsplaining.shared.utils import (
     get_policy_name,
     is_aws_managed,
 )
+
+if TYPE_CHECKING:
+    from cloudsplaining.scan.managed_policy_detail import ManagedPolicyDetails
+    from cloudsplaining.scan.statement_detail import StatementDetail
 
 
 class GroupDetailList:
