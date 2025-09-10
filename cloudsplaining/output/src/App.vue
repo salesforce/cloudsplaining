68d9b92b832ccb3b8b4b6fbef637031fa3c1bc68
Leona Magaya <leonatmagaya@gmail.com>
2025-09-10 10:57:14 +0200

    feat: Add custom guidance and appendices support
    - Add TemplateConfig class to detect and process custom HTML files
    - Support custom-guidance.html and custom-appendices.html in project root
    - Implement dynamic navigation with conditional tab visibility
    - Add Vue components for custom content rendering with v-html
    - Include comprehensive test coverage for all scenarios
    - Update documentation with usage examples and behavior guide

--- cloudsplaining/output/src/App.vue
+++ cloudsplaining/output/src/App.vue
@@ -1,23 +1,33 @@
 <template>
     <div id="main">
         <b-navbar toggleable="md" variant="faded">
-            <b-navbar-brand to="/summary">
-                Cloudsplaining
-            </b-navbar-brand>
+            <b-navbar-brand to="/summary"> Cloudsplaining </b-navbar-brand>
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>
-                    <b-nav-item to="/customer-policies">Customer Policies</b-nav-item>
-                    <b-nav-item to="/inline-policies">Inline Policies</b-nav-item>
+                    <b-nav-item to="/customer-policies"
+                        >Customer Policies</b-nav-item
+                    >
+                    <b-nav-item to="/inline-policies"
+                        >Inline Policies</b-nav-item
+                    >
                     <b-nav-item to="/aws-policies">AWS Policies</b-nav-item>
                     <b-nav-item to="/iam-principals">IAM Principals</b-nav-item>
-                    <b-nav-item to="/guidance">Guidance</b-nav-item>
-                    <b-nav-item to="/appendices">Appendices</b-nav-item>
+                    <b-nav-item v-if="show_guidance_nav" to="/guidance"
+                        >Guidance</b-nav-item
+                    >
+                    <b-nav-item v-if="show_appendices_nav" to="/appendices"
+                        >Appendices</b-nav-item
+                    >
                     <!-- <b-nav-item to="/task-table">Task Table Demo</b-nav-item> -->
                 </b-navbar-nav>
                 <b-navbar-nav class="ml-auto">
-                    <b-nav-text><strong>Account ID:</strong> {{ account_id }} | <strong>Account Name:</strong> {{ account_name }}</b-nav-text>
+                    <b-nav-text
+                        ><strong>Account ID:</strong> {{ account_id }} |
+                        <strong>Account Name:</strong>
+                        {{ account_name }}</b-nav-text
+                    >
                 </b-navbar-nav>
             </b-collapse>
         </b-navbar>
@@ -25,20 +35,20 @@
         <b-container class="mt-3 pb-3 report">
             <b-tabs nav-class="d-none">
                 <router-view />
-               <!-- <b-tab key="task-table">-->
-<!--                    <br>-->
-<!--                    <h3>Tasks (demo WIP)</h3>-->
-<!--                    <br>-->
-<!--&lt;!&ndash;                    <h3>Customer-Managed Policies</h3>&ndash;&gt;-->
-<!--&lt;!&ndash;                    <TaskTable managedBy="Customer" v-bind:items_mapping="getTaskTableMapping('Customer')"/>&ndash;&gt;-->
-<!--&lt;!&ndash;                    <br>&ndash;&gt;-->
-<!--                    &lt;!&ndash;TODO: Figure out the overlap issue where the two tables results in a double info field in Customer policies&ndash;&gt;-->
-<!--                    <h3>AWS-Managed Policies</h3>-->
-<!--                    <TaskTable managedBy="AWS" v-bind:items_mapping="getTaskTableMapping('AWS')"/>-->
-<!--                    &lt;!&ndash;TODO: Task table for Inline Policies&ndash;&gt;-->
-<!--&lt;!&ndash;                    <h3>Inline Policies</h3>&ndash;&gt;-->
-<!--&lt;!&ndash;                    <TaskTable v-bind:policyNameMapping="getInlinePolicyNameMapping()"/>&ndash;&gt;-->
-<!--                </b-tab> -->
+                <!-- <b-tab key="task-table">-->
+                <!--                    <br>-->
+                <!--                    <h3>Tasks (demo WIP)</h3>-->
+                <!--                    <br>-->
+                <!--&lt;!&ndash;                    <h3>Customer-Managed Policies</h3>&ndash;&gt;-->
+                <!--&lt;!&ndash;                    <TaskTable managedBy="Customer" v-bind:items_mapping="getTaskTableMapping('Customer')"/>&ndash;&gt;-->
+                <!--&lt;!&ndash;                    <br>&ndash;&gt;-->
+                <!--                    &lt;!&ndash;TODO: Figure out the overlap issue where the two tables results in a double info field in Customer policies&ndash;&gt;-->
+                <!--                    <h3>AWS-Managed Policies</h3>-->
+                <!--                    <TaskTable managedBy="AWS" v-bind:items_mapping="getTaskTableMapping('AWS')"/>-->
+                <!--                    &lt;!&ndash;TODO: Task table for Inline Policies&ndash;&gt;-->
+                <!--&lt;!&ndash;                    <h3>Inline Policies</h3>&ndash;&gt;-->
+                <!--&lt;!&ndash;                    <TaskTable v-bind:policyNameMapping="getInlinePolicyNameMapping()"/>&ndash;&gt;-->
+                <!--                </b-tab> -->
             </b-tabs>
         </b-container>
         <b-container>
@@ -46,7 +56,10 @@
                 <b-col class="text-center text-muted">
                     Report Generated: {{ report_generated_time }} &diamond;
                     Cloudsplaining version:
-                    <b-link href="https://github.com/salesforce/cloudsplaining">{{ cloudsplaining_version }}</b-link>
+                    <b-link
+                        href="https://github.com/salesforce/cloudsplaining"
+                        >{{ cloudsplaining_version }}</b-link
+                    >
                 </b-col>
             </b-row>
         </b-container>
@@ -54,121 +67,131 @@
 </template>
 
 <script>
-    const managedPoliciesUtil = require('./util/managed-policies');
-    const inlinePoliciesUtil = require('./util/inline-policies');
-    const taskTableUtil = require('./util/task-table');
-    const sampleData = require('./sampleData');
+const managedPoliciesUtil = require("./util/managed-policies");
+const inlinePoliciesUtil = require("./util/inline-policies");
+const taskTableUtil = require("./util/task-table");
+const sampleData = require("./sampleData");
 
-    console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`)
+console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`);
 
-    // This conditionally loads the local sample data if you are developing, but not if you are viewing the report
+// This conditionally loads the local sample data if you are developing, but not if you are viewing the report
 
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV === "development" || isLocalExample === true) {
     // eslint-disable-next-line no-undef
-    if ((process.env.NODE_ENV === "development") || (isLocalExample === true)) {
-        // eslint-disable-next-line no-undef
-        console.log(`isLocalExample is set to: ${isLocalExample}`)
-        console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`)
-        console.log(`Note: a report generated with the Python template will not have isLocalExample set to True, because that uses a separate template.html file, which has isLocalExample set to False.`)
-        // eslint-disable-next-line no-unused-vars,no-undef
-        iam_data = sampleData.sample_iam_data;
-        // eslint-disable-next-line no-undef
-        console.log(`IAM Data keys inside the development if statement: ${Object.keys(iam_data)}`);
-        // eslint-disable-next-line no-unused-vars,no-undef
-        account_id = "12345678912";
-        // eslint-disable-next-line no-unused-vars,no-undef
-        account_name = "example";
-        // eslint-disable-next-line no-unused-vars,no-undef
-        report_generated_time = "2020-09-01";
-        // eslint-disable-next-line no-unused-vars,no-undef
-        cloudsplaining_version = "0.2.2";
-    }
-    else {
-        // eslint-disable-next-line no-undef
-        console.log(`isLocalExample is set to: ${isLocalExample}`)
-    }
+    console.log(`isLocalExample is set to: ${isLocalExample}`);
+    console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`);
+    console.log(
+        `Note: a report generated with the Python template will not have isLocalExample set to True, because that uses a separate template.html file, which has isLocalExample set to False.`
+    );
+    // eslint-disable-next-line no-unused-vars,no-undef
+    iam_data = sampleData.sample_iam_data;
 
+    console.log(
+        `IAM Data keys inside the development if statement: ${Object.keys(
+            // eslint-disable-next-line no-undef
+            iam_data
+        )}`
+    );
+    // eslint-disable-next-line no-unused-vars,no-undef
+    account_id = "12345678912";
+    // eslint-disable-next-line no-unused-vars,no-undef
+    account_name = "example";
+    // eslint-disable-next-line no-unused-vars,no-undef
+    report_generated_time = "2020-09-01";
+    // eslint-disable-next-line no-unused-vars,no-undef
+    cloudsplaining_version = "0.2.2";
+} else {
     // eslint-disable-next-line no-undef
-    console.log(`IAM Data keys outside of the NODE_ENV if statements: ${Object.keys(iam_data)}`);
+    console.log(`isLocalExample is set to: ${isLocalExample}`);
+}
 
-
-    function getManagedPolicyNameMapping(managedBy) {
+console.log(
+    `IAM Data keys outside of the NODE_ENV if statements: ${Object.keys(
         // eslint-disable-next-line no-undef
-        return managedPoliciesUtil.getManagedPolicyNameMapping(iam_data, managedBy)
-    }
+        iam_data
+    )}`
+);
 
-    function getInlinePolicyNameMapping() {
-        // eslint-disable-next-line no-undef
-        return inlinePoliciesUtil.getInlinePolicyNameMapping(iam_data)
-    }
+function getManagedPolicyNameMapping(managedBy) {
+    // eslint-disable-next-line no-undef
+    return managedPoliciesUtil.getManagedPolicyNameMapping(iam_data, managedBy);
+}
 
-    function getTaskTableMapping(managedBy) {
-        // eslint-disable-next-line no-undef
-        return taskTableUtil.getTaskTableMapping(iam_data, managedBy)
-    }
+function getInlinePolicyNameMapping() {
+    // eslint-disable-next-line no-undef
+    return inlinePoliciesUtil.getInlinePolicyNameMapping(iam_data);
+}
 
+function getTaskTableMapping(managedBy) {
+    // eslint-disable-next-line no-undef
+    return taskTableUtil.getTaskTableMapping(iam_data, managedBy);
+}
 
-    export default {
-        name: 'App',
-        mounted: function()
-        {
-            // Workaround for handling anchors when vue-router is in hash mode
-            // https://stackoverflow.com/a/45206192
-            setTimeout(() => this.scrollFix(this.$route.hash), 1);
+export default {
+    name: "App",
+    mounted: function () {
+        // Workaround for handling anchors when vue-router is in hash mode
+        // https://stackoverflow.com/a/45206192
+        setTimeout(() => this.scrollFix(this.$route.hash), 1);
+    },
+    data() {
+        return {
+            // eslint-disable-next-line no-undef
+            sharedState: iam_data,
+            // eslint-disable-next-line no-undef
+            account_id: account_id,
+            // eslint-disable-next-line no-undef
+            account_name: account_name,
+            // eslint-disable-next-line no-undef
+            report_generated_time: report_generated_time,
+            // eslint-disable-next-line no-undef
+            cloudsplaining_version: cloudsplaining_version,
+            // eslint-disable-next-line no-undef
+            show_guidance_nav: show_guidance_nav === "True",
+            // eslint-disable-next-line no-undef
+            show_appendices_nav: show_appendices_nav === "True",
+        };
+    },
+    computed: {
+        iam_data() {
+            return this.sharedState;
         },
-        data() {
-            return {
-                // eslint-disable-next-line no-undef
-                sharedState: iam_data,
-                // eslint-disable-next-line no-undef
-                account_id: account_id,
-                // eslint-disable-next-line no-undef
-                account_name: account_name,
-                // eslint-disable-next-line no-undef
-                report_generated_time: report_generated_time,
-                // eslint-disable-next-line no-undef
-                cloudsplaining_version: cloudsplaining_version,
-            };
+    },
+    methods: {
+        getManagedPolicyNameMapping: function (managedBy) {
+            return getManagedPolicyNameMapping(managedBy);
         },
-        computed: {
-            iam_data() {
-                return this.sharedState
-            }
+        getInlinePolicyNameMapping: function () {
+            return getInlinePolicyNameMapping();
         },
-        methods: {
-            getManagedPolicyNameMapping: function (managedBy) {
-                return getManagedPolicyNameMapping(managedBy)
-            },
-            getInlinePolicyNameMapping: function () {
-                return getInlinePolicyNameMapping()
-            },
-            getTaskTableMapping: function (managedBy) {
-                return getTaskTableMapping(managedBy)
-            },
-            scrollFix: function(hashbang)
-            {
-                location.hash = hashbang;
-            }
+        getTaskTableMapping: function (managedBy) {
+            return getTaskTableMapping(managedBy);
         },
-        provide() {
-            return {
-                iam_data: this.iam_data,
-                getManagedPolicyNameMapping: this.getManagedPolicyNameMapping,
-                getInlinePolicyNameMapping: this.getInlinePolicyNameMapping,
-            }
-        }
-    }
+        scrollFix: function (hashbang) {
+            location.hash = hashbang;
+        },
+    },
+    provide() {
+        return {
+            iam_data: this.iam_data,
+            getManagedPolicyNameMapping: this.getManagedPolicyNameMapping,
+            getInlinePolicyNameMapping: this.getInlinePolicyNameMapping,
+        };
+    },
+};
 </script>
 
 <style>
-    #main {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: left;
-        color: #2c3e50;
-    }
+#main {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: left;
+    color: #2c3e50;
+}
 
-    .router-link-exact-active {
-        font-weight: bold;
-    }
+.router-link-exact-active {
+    font-weight: bold;
+}
 </style>
---
+++ test/shared/test_template_config.py
@@ -0,0 +1,74 @@
+import unittest
+import os
+import tempfile
+import shutil
+from pathlib import Path
+from cloudsplaining.shared.template_config import TemplateConfig
+
+
+class TemplateConfigTestCase(unittest.TestCase):
+    def setUp(self):
+        """Set up temporary directory for each test"""
+        self.original_cwd = os.getcwd()
+        self.temp_dir = tempfile.mkdtemp()
+        os.chdir(self.temp_dir)
+
+    def tearDown(self):
+        """Clean up after each test"""
+        os.chdir(self.original_cwd)
+        shutil.rmtree(self.temp_dir)
+
+    def test_no_custom_files_returns_default(self):
+        """When no custom files exist, should return default content"""
+        config = TemplateConfig()
+
+        self.assertEqual(config.guidance_content, "default")
+        self.assertEqual(config.appendices_content, "default")
+        self.assertTrue(config.show_guidance_nav)
+        self.assertTrue(config.show_appendices_nav)
+
+    def test_custom_files_with_content(self):
+        """Custom files with content should be processed correctly"""
+        Path("custom-guidance.html").write_text("<h1>Custom Guidance</h1>", encoding="utf-8")
+        Path("custom-appendices.html").write_text("<h1>Custom Appendices</h1>", encoding="utf-8")
+
+        config = TemplateConfig()
+
+        self.assertEqual(config.guidance_content, "<h1>Custom Guidance</h1>")
+        self.assertEqual(config.appendices_content, "<h1>Custom Appendices</h1>")
+        self.assertTrue(config.show_guidance_nav)
+        self.assertTrue(config.show_appendices_nav)
+
+    def test_empty_custom_files_hide_navigation(self):
+        """Empty custom files should hide navigation tabs"""
+        Path("custom-guidance.html").write_text("", encoding="utf-8")
+        Path("custom-appendices.html").write_text("   ", encoding="utf-8")
+
+        config = TemplateConfig()
+
+        self.assertEqual(config.guidance_content, "")
+        self.assertEqual(config.appendices_content, "")
+        self.assertFalse(config.show_guidance_nav)
+        self.assertFalse(config.show_appendices_nav)
+
+    def test_javascript_string_escaping(self):
+        """HTML content should be properly escaped for JavaScript"""
+        content_with_special_chars = '<h1>Test "quoted" content</h1>\n<p>New line</p>'
+        Path("custom-guidance.html").write_text(content_with_special_chars, encoding="utf-8")
+
+        config = TemplateConfig()
+
+        self.assertIn('\\"', config.guidance_content)
+        self.assertIn("\\n", config.guidance_content)
+        self.assertNotIn("\n", config.guidance_content)
+
+    def test_mixed_scenario_one_custom_one_default(self):
+        """Should handle mixed scenario with one custom file and one default"""
+        Path("custom-guidance.html").write_text("<h1>Only Guidance</h1>", encoding="utf-8")
+
+        config = TemplateConfig()
+
+        self.assertEqual(config.guidance_content, "<h1>Only Guidance</h1>")
+        self.assertEqual(config.appendices_content, "default")
+        self.assertTrue(config.show_guidance_nav)
+        self.assertTrue(config.show_appendices_nav)
